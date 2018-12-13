db0a863927feaf9187064944ba21a498dbd755c9
bisonhubert <bisonhubert@gmail.com>
2018-12-13 12:51:21 -0800

    Display list of comics

--- src/App.js
+++ src/App.js
@@ -21,9 +21,8 @@ class App extends Component {
     fetch(newComicsUrl)
       .then(response => response.json())
       .then(data => {
-        console.log(data)
         this.setState({
-          newComics: data
+          newComics: data.comics
         })
       })
   }
@@ -33,18 +32,35 @@ class App extends Component {
     fetch(futureComicsUrl)
       .then(response => response.json())
       .then(data => {
-        console.log(data)
         this.setState({
-          futureComics: data
+          futureComics: data.comics
         })
       })
   }
 
   render() {
+    // props.comic.creators: "(W) Sonny Liew, Ted Anderson (A) Marina Julia (CA) Jon Vermilyea"
+    // props.comic.description: ""
+    // props.comic.diamond_id: "SEP188086"
+    // props.comic.price: "$3.99"
+    // props.comic.publisher: "BOOM! STUDIOS"
+    // props.comic.release_date: "2018-12-12"
+    // props.comic.title: "ADVENTURE TIME SEASON 11 #1 (2ND PTG)"
+
+    // todo: get the images from each of these
+    // possibly by scraping from different publisher sites
+    const NewComics = this.state.newComics.map(comic => {
+      console.log(comic)
+      return <ComicBook key={comic.diamond_id} comic={comic} />
+    })
+    const FutureComics = this.state.futureComics.map(comic => {
+      return <ComicBook key={comic.diamond_id} comic={comic} />
+    })
     return (
       <div className="App">
         <h1>Bison Hubert</h1>
-        <ComicBook />
+        {NewComics}
+        {FutureComics}
       </div>
     );
   }
--- src/components/comic-calendar/ComicBook.js
+++ src/components/comic-calendar/ComicBook.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default function ComicBook() {
+export default function ComicBook(props) {
+  // props.comic.creators: "(W) Sonny Liew, Ted Anderson (A) Marina Julia (CA) Jon Vermilyea"
+  // props.comic.description: ""
+  // props.comic.diamond_id: "SEP188086"
+  // props.comic.price: "$3.99"
+  // props.comic.publisher: "BOOM! STUDIOS"
+  // props.comic.release_date: "2018-12-12"
+  // props.comic.title: "ADVENTURE TIME SEASON 11 #1 (2ND PTG)"
+
+  // todo: get the images from each of these
+  // possibly by scraping from different publisher sites
   return (
     <article>
-      Hi
+      <h2>{props.comic.title}</h2>
+      <h3>{props.comic.description}</h3>
+      <h4>{props.comic.publisher}</h4>
+      <h5>{props.comic.price}</h5>
+      <h6>{props.comic.release_date}</h6>
     </article>
   )
 }
