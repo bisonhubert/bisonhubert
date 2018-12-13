329249063aa6407a2c0b4305c273f08c8ca3e536
bisonhubert <bisonhubert@gmail.com>
2018-12-13 12:39:06 -0800

    Add new ComicBook component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import ComicBook from './components/comic-calendar/ComicBook'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      newComics: [],
+      futureComics: []
+    }
+  }
+
+  componentDidMount() {
+    this.getNewComics()
+    this.getFutureComics()
+  }
+
+  getNewComics() {
+    const newComicsUrl = 'https://api.shortboxed.com/comics/v1/new'
+    fetch(newComicsUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          newComics: data
+        })
+      })
+  }
+
+  getFutureComics() {
+    const futureComicsUrl = 'https://api.shortboxed.com/comics/v1/future'
+    fetch(futureComicsUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          futureComics: data
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Bison Hubert</h1>
+        <ComicBook />
       </div>
     );
   }
---
+++ src/components/comic-calendar/ComicBook.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default function ComicBook() {
+  return (
+    <article>
+      Hi
+    </article>
+  )
+}
