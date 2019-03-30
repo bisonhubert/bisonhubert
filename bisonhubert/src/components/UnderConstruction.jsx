0ebcb5989b96288340d42fb49ec2d673bd8baba2
Bison Hubert <bisonhubert@gmail.com>
2019-03-30 08:42:58 -0700

    Style on desktop

--- bisonhubert/src/components/UnderConstruction.jsx
+++ bisonhubert/src/components/UnderConstruction.jsx
@@ -11,9 +11,9 @@ class UnderConstruction extends Component {
   render() {
     return (
       <div style={{backgroundColor: randomBkgColor}} className="h--100p w--100p">
-        <div className="m--0-auto p--relative t--calc--50p-280-5 w--300px">
+        <div className="m--0-auto p--relative t--calc--50p-280-5 w--300px ai--center--at-724px d--flex--at-724px jc--center--at-724px t--calc--50p-97--at-724px w--auto--at-724px">
           <img src={cones} className="h--130px mt---4px" alt="Pikachu placing cones on the ground"/>
-          <div className="h--194px l--3px ovf--hidden mb--50px mt--50px p--relative w--294px">
+          <div className="d--ib h--194px l--3px ovf--hidden p--relative w--294px mb--50px---max-723px mt--50px---max-723px ml--7p--at-724px mr--7p--at-724px ml--10p--at-1024px mr--10p--at-1024px">
             <img src={jackhammer} className="l---3px p--relative t---3px" alt="Pikachu jackhammering" />
           </div>
           <img src={cones} className="h--130px mt---4px" alt="Pikachu placing cones on the ground" />
