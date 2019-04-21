bb2919c6531b3a561919653091dc521f9f79f86f
Bison Hubert <bisonhubert@gmail.com>
2019-04-21 14:05:57 -0700

    Add new React app w/ under construction component (#2)
    * Add local setup steps
    * Restart project
    * Add under construction component
    * Style construction components for mobile
    * Add dynamic background colors
    * Style on desktop
    * Rename utility stylesheet
    * Fix background randomizer n+1 bug
    * Add instant page for just-in-time preloading
    * Add site title
    * Add favicons
    * Add package lock

---
+++ bisonhubert/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import UnderConstruction from './components/UnderConstruction.jsx'
+import './App.css';
+import './utilities.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App h--100vh w--100vw">
+        <UnderConstruction />
+      </div>
+    );
+  }
+}
+
+export default App;
