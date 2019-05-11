3187a021cf679f47c7b569aeffe7d02d59f52060
Bison Hubert <bisonhubert@gmail.com>
2019-05-11 14:52:23 -0700

    Add nav tiles

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import NavTile from './NavTile';
+
 function Landing() {
   return (
-    <div>
-
+    <div className="Landing h--100p w--100p">
+      <NavTile id='NavTile--red' />
+      <NavTile id='NavTile--yellow' />
+      <NavTile id='NavTile--green' />
+      <NavTile id='NavTile--blue' />
     </div>
   )
 }
---
+++ src/components/NavTile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavTile(props) {
+  return (
+    <div id={props.id} className="NavTile">
+      <a href="/" className="">
+        <article className="Tile">
+        </article>
+      </a>
+    </div>
+  )
+}
+
+export default NavTile
