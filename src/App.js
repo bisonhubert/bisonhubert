cf3604f9df70e0a9d82e4607dffe250f0245555f
Bison Hubert <bisonhubert@gmail.com>
2019-05-11 10:16:31 -0700

    Cleanup files and reset css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import UnderConstruction from './components/UnderConstruction.jsx'
-import './App.css';
-import './utilities.css';
+
+import './stylesheets/app.css';
+import './stylesheets/utilities.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App h--100vh w--100vw">
-        <UnderConstruction />
+
       </div>
     );
   }
