6d463d69146c462e516cecdb9c3fa761c5f6718e
Bison Hubert <bisonhubert@gmail.com>
2019-05-20 17:09:49 -0600

    Add WIP in progress section

--- src/components/landing/InProgress.jsx
+++ src/components/landing/InProgress.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 function InProgress() {
   return (
-    <div>
-      I'm the InProgress block
+    <div className="in-progress">
+      <h3>Comics Calendar</h3>
+      <p>As a huge comic book fan, I wanted a way to view my upcoming <a href="">pull list</a> without having to track each title down. I created a small command line program that suited my needs, but I felt it was too limited to my own needs. So I've started creating a single-page web application that will read from Image, DC, Marvel, and Dark Horse comic release calendars and allow you to sort through them, define your pull list, and export it to iCal or Google Calendar. Initial mockups are available below. The code for the original CLI program is still up on <a href="" target="_blank" rel="noopener">Github</a>.
+      </p>
+      <article className="screenshot">
+
+      </article>
     </div>
   )
 }
