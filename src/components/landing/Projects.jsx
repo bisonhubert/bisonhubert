2db15095d1059a3bb275acdc9a39cf05daf5bd96
Bison Hubert <bisonhubert@gmail.com>
2019-05-20 17:01:46 -0600

    Add WIP projects

--- src/components/landing/Projects.jsx
+++ src/components/landing/Projects.jsx
@@ -2,8 +2,47 @@ import React from 'react';
 
 function Projects() {
   return (
-    <div>
-      I'm the Projects block
+    <div id="projects">
+      <article id="generocity">
+        <h3>Generocity</h3>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <a href="/" target="_blank" rel="noopener">More --></a>
+        </p>
+        <article className="screenshots">
+
+        </article>
+      </article>
+      <article id="cool-dragon">
+        <h3>Cool Dragon</h3>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <a href="/" target="_blank" rel="noopener">More --></a>
+        </p>
+        <article className="screenshots">
+
+        </article>
+      </article>
+      <article id="gains">
+        <h3>Gains</h3>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <a href="/" target="_blank" rel="noopener">More --></a>
+        </p>
+        <article className="screenshots">
+
+        </article>
+      </article>
+      <article id="reddit-clone">
+        <h3>Reddit Clone</h3>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <a href="/" target="_blank" rel="noopener">More --></a>
+        </p>
+        <article className="screenshots">
+
+        </article>
+      </article>
     </div>
   )
 }
