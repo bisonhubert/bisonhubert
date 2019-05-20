924cc738ff8eeaafba1fc201e8f2811e107daf91
Bison Hubert <bisonhubert@gmail.com>
2019-05-20 16:30:12 -0600

    Add about block with placeholder copy

--- src/components/landing/About.jsx
+++ src/components/landing/About.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 function About() {
   return (
     <div>
-      I'm the About block
+      <p>
+        I’m an Oakland based software engineer, working on the eCommerce marketplace and membership services at Brava. Something about mentoring. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <a href="/">More --></a>
+      </p>
     </div>
   )
 }
