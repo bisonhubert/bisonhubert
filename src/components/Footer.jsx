cb2328afcae5c867befb674e0a440873a04af717
Bison Hubert <bisonhubert@gmail.com>
2019-05-20 16:10:31 -0600

    Add footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import Logo from './Logo'
+
 function Footer() {
   return (
-    <div>
-      I'm the Footer
-    </div>
+    <footer>
+      <article>
+        <nav>
+          <article>
+            <a href="">Email me</a>
+          </article>
+          <article>
+            <a href="">Twitter</a>
+          </article>
+          <article>
+            <a href="">LinkedIn</a>
+          </article>
+        </nav>
+      </article>
+      <article>
+        <Logo />
+        <p>
+          <span>Bison Hubert, 2019</span>
+          <span>|</span>
+          <span>View on Github</span>
+        </p>
+      </article>
+    </footer>
   )
 }
 
