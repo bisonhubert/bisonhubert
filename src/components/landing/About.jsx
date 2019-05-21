a807fc7434688e4076cc01ef13599ffe43a07bf9
Bison Hubert <bisonhubert@gmail.com>
2019-05-20 21:17:47 -0700

    Update more arrow to hex value

--- src/components/landing/About.jsx
+++ src/components/landing/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div>
       <p>
         I’m an Oakland based software engineer, working on the eCommerce marketplace and membership services at Brava. Something about mentoring. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        <a href="">More --></a>
+        <a href="">More &#x2192;</a>
       </p>
     </div>
   )
