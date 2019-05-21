fc68c85c7fc9a8d8f5d66773be35e7da17d111cc
Bison Hubert <bisonhubert@gmail.com>
2019-05-20 21:19:56 -0700

    Hide inactive header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,14 +9,19 @@ function Header() {
         <Logo />
         <article className="site-nav">
           <article className="site-nav__link">
-            <a href="">Projects</a>
+            <a href="#projects">Projects</a>
           </article>
+          { /*
+            add blog when first blog post is ready
           <article className="site-nav__link">
             <a href="">Blog</a>
           </article>
+
+            add about when page is ready
           <article className="site-nav__link">
             <a href="">About</a>
           </article>
+          */ }
         </article>
       </nav>
     </header>
