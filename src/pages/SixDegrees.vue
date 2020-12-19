bbab05bede7d1bb7af0984abb90cb05e0a97ca9f
Bison Hubert <bisonhubert@gmail.com>
2020-12-18 23:57:36 -0800

    Six degrees todo

---
+++ src/pages/SixDegrees.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Uses Twitter bot 1. 2 field form w/ one actor in each 2. Generates Tweet on
+    my profile 3. Comments bot kickoff to show all permutations up to 6 films
+    MVP + 1. Validate actors names before submitting
+  </div>
+</template>
