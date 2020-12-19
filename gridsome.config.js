c68173fa090e04b3d7b59c809695c5da12b9ac93
Bison Hubert <bisonhubert@gmail.com>
2020-12-18 23:14:27 -0800

    Add blog template

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,33 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
   siteName: 'BB',
-  plugins: [],
+  siteUrl: 'https://bisonhubert.com',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true,
+          },
+        },
+      },
+    },
+  ],
+  templates: {
+    Tag: '/tag/:id',
+    Post: '/blog/:path',
+  },
+  transformers: {
+    remark: {
+      autolinkClassName: 'icon icon-link heading-anchor',
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['noopener'],
+      anchorClassName: 'icon icon-link',
+    },
+  },
 };
