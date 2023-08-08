99ce48270d801ca26bca5f4624b3f885efb5121d
Bison Hubert <bisonhubert@gmail.com>
2023-08-08 16:50:05 -0700

    Add landing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Bison rules.
-      </header>
+    <div>
+      <main>
+        <article>
+          <p>Bison Hubert</p>
+          <p>Software Engineer + Mentor</p>
+          <p>Reno, NV</p>
+        </article>
+      </main>
     </div>
   );
 }
