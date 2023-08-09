23ea7f2b77cc71a00190f4b0ffc7a4aee8e9c006
Bison Hubert <bisonhubert@gmail.com>
2023-08-08 17:09:32 -0700

    Link to about page

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,15 @@
+// todobison work with a layout
+
+import { Outlet, Link } from "react-router-dom";
+
 function Root() {
   return (
     <div>
+      <header>
+        <nav>
+          <Link to={`about`}>About</Link>
+        </nav>
+      </header>
       <main>
         <article>
           <p>Bison Hubert</p>
